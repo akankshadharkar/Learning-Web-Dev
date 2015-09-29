bf3b0d139418b8e4bce340df59b82a9ff6c3f99b
Akanksha Dharkar <akankshadharkar@gmail.com>
2015-09-30 01:02:54 +0530

    Made the first basic files

---
+++ myjs.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+{
+	
+	$('button#mybtn').on('click',function()
+	{
+		//var akuText=$('Kana').text();
+		//alert('HELLO');
+		alert('Hey there, notice the text');
+		$('#Kana').text('My name is Akanksha Dharkar');
+	});
+});
